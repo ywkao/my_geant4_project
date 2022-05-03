d7d6e1f6cce8df062885c959cf7345b110b15ec1
ykao <ykao@cern.ch>
2022-05-03 12:51:32 +0800

    G4PVReplica: trials with discs

--- B1_v3/src/DetectorConstruction.cc
+++ B1_v3/src/DetectorConstruction.cc
@@ -93,89 +93,34 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
   G4Box* solidEnv = new G4Box("Envelope", 0.5*env_sizeXY, 0.5*env_sizeXY, 0.5*env_sizeZ);
   G4LogicalVolume* logicEnv = new G4LogicalVolume(solidEnv, env_mat, "Envelope");
   new G4PVPlacement(0, G4ThreeVector(), logicEnv, "Envelope", logicWorld, false, 0, checkOverlaps);
-
   fScoringVolume = logicEnv;
-
-  // //++++++++++++++++++++++++++++++
-  // // Shape 1
-  // //++++++++++++++++++++++++++++++
-  // //G4Material* shape1_mat = nist->FindOrBuildMaterial("G4_A-150_TISSUE");
-  // G4Material* shape1_mat = nist->FindOrBuildMaterial("G4_Si");
-  // G4ThreeVector pos1 = G4ThreeVector(0, 0, -7*cm);
-
-  // // Conical section shape
-  // G4double shape1_rmina =  0.*cm, shape1_rmaxa = 2.*cm;
-  // G4double shape1_rminb =  0.*cm, shape1_rmaxb = 4.*cm;
-  // G4double shape1_hz = 3.*cm;
-  // G4double shape1_phimin = 0.*deg, shape1_phimax = 360.*deg;
-  // G4Cons* solidShape1 = new G4Cons("Shape1", shape1_rmina, shape1_rmaxa, shape1_rminb, shape1_rmaxb, shape1_hz, shape1_phimin, shape1_phimax);
-  // G4LogicalVolume* logicShape1 = new G4LogicalVolume(solidShape1, shape1_mat, "Shape1");
-  // new G4PVPlacement(0, pos1, logicShape1, "Shape1", logicEnv, false, 0, checkOverlaps);
-
-  // //++++++++++++++++++++++++++++++
-  // // Shape 2
-  // //++++++++++++++++++++++++++++++
-  // G4Material* shape2_mat = nist->FindOrBuildMaterial("G4_BONE_COMPACT_ICRU");
-  // G4ThreeVector pos2 = G4ThreeVector(0, 0, 7*cm);
-
-  // // Trapezoid shape
-  // G4double shape2_dxa = 12*cm, shape2_dxb = 12*cm;
-  // G4double shape2_dya = 10*cm, shape2_dyb = 16*cm;
-  // G4double shape2_dz  = 6*cm;
-  // G4Trd* solidShape2 = new G4Trd("Shape2", 0.5*shape2_dxa, 0.5*shape2_dxb, 0.5*shape2_dya, 0.5*shape2_dyb, 0.5*shape2_dz);
-  // G4LogicalVolume* logicShape2 = new G4LogicalVolume(solidShape2, shape2_mat, "Shape2");
-  // new G4PVPlacement(0, pos2, logicShape2, "Shape2", logicEnv, false, 1, checkOverlaps);
-
-  // // Set Shape2 as scoring volume
-  // fScoringVolume = logicShape2;
 
   //++++++++++++++++++++
   // disc
   //++++++++++++++++++++
   // c. Create a target disc (a full tube), defining first its dimensions:
-   G4double discZLength = 0.1*mm;
-   G4double discRadius  = 3*cm;
+  G4double discZLength = 0.1*mm;
+  G4double discRadius  = 3*cm;
 
-   // tube = name, min radius, max radius, z half length, start phi, delta phi
-   G4VSolid* tube 
-     = new G4Tubs("tube", 0, 2*discRadius, 0.5*env_sizeZ, 0, twopi*rad);
-   G4LogicalVolume* tube_log
-     = new G4LogicalVolume(tube, env_mat, "tubeL");   // name
-   //G4VPhysicalVolume* tube_phy =
-   //    new G4PVPlacement(nullptr, G4ThreeVector(0., 0., 0.), tube_log, "tubeP", logicWorld, false, 2);
+  // tube = name, min radius, max radius, z half length, start phi, delta phi
+  G4VSolid* tube = new G4Tubs("tube", 0, 2*discRadius, 0.5*env_sizeZ, 0, twopi*rad);
+  G4LogicalVolume* tube_log = new G4LogicalVolume(tube, env_mat, "tubeL");   // name
+  //new G4PVPlacement(nullptr, G4ThreeVector(0., 0., 0.), tube_log, "tubeP", logicEnv, false, 2);
   
-   G4VSolid* disc 
-     = new G4Tubs("disc-Target", 0, discRadius, discZLength, 0, twopi*rad);
-   G4LogicalVolume* disc_log
-     = new G4LogicalVolume(disc, material_silicon, "logic-Disc");
-   //G4VPhysicalVolume* discPhysical =
-   //    new G4PVReplica("detector", disc_log, tube_log, kZAxis, 4, discZLength*1.1);
+  G4VSolid* disc = new G4Tubs("disc-Target", 0, 0.5*discRadius, discZLength, 0, twopi*rad);
+  G4LogicalVolume* disc_log = new G4LogicalVolume(disc, material_silicon, "logic-Disc");
+  //new G4PVReplica("detector", disc_log, tube_log, kXAxis, 4, discZLength*1.1);
+  //new G4PVReplica("detector", disc_log, tube_log, kYAxis, 4, discZLength*1.1);
+  //new G4PVReplica("detector", disc_log, tube_log, kZAxis, 4, discZLength*1.1);
+  //new G4PVReplica("detector", disc_log, logicEnv, kZAxis, 4, discZLength*1.1);
+  //new G4PVReplica("detector", disc_log, physWorld, kZAxis, 4, discZLength*1.1);
 
-   G4double init_position = -2.25*cm;
-   for(int i=0; i<10; ++i) {
-       G4double increment = (double)i * 0.5*cm;
-       G4double zposition = init_position + increment;
-       new G4PVPlacement(nullptr, G4ThreeVector(0.5*cm, 0.5*cm, zposition), disc_log, "Disc", logicEnv, false, i+2, true);
-   }
-
-    // //new G4PVPlacement(nullptr,                 // (no) rotation
-    // //		     G4ThreeVector(0.5*cm, 0.5*cm, 0.5*cm), // translation
-    // //		     disc_log,             // its logical volume
-    // //		     "Disc",                  // its name
-    // //		     logicEnv,            // its mother volume
-    // //		     false,                   // not used
-    // //		     2,                       // copy number
-    // //		     true);                   // enable overlap check
-
-    // //new G4PVPlacement(nullptr,                 // (no) rotation
-    // //		     G4ThreeVector(0.5*cm, 0.5*cm, -0.5*cm), // translation
-    // //		     disc_log,             // its logical volume
-    // //		     "Disc",                  // its name
-    // //		     logicEnv,            // its mother volume
-    // //		     false,                   // not used
-    // //		     3,                       // copy number
-    // //		     true);                   // enable overlap check
-
+  G4double init_position = -2.25*cm;
+  for(int i=0; i<10; ++i) {
+      G4double increment = (double)i * 0.5*cm;
+      G4double zposition = init_position + increment;
+      new G4PVPlacement(nullptr, G4ThreeVector(0.5*cm, 0.5*cm, zposition), disc_log, "Disc", logicEnv, false, i+2, true);
+  }
 
   //++++++++++++++++++++++++++++++
   //always return the physical World
